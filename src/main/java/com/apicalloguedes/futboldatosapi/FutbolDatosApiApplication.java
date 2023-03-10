0f1ba1f35418a045312f7df91484c28dd4422bba
mpicallo <mapicalloperez@hotmail.com>
2023-03-10 12:05:54 +0100

    proyecto base spring boot

---
+++ src/main/java/com/apicalloguedes/futboldatosapi/FutbolDatosApiApplication.java
@@ -0,0 +1,13 @@
+package com.apicalloguedes.futboldatosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutbolDatosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutbolDatosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apicalloguedes/futboldatosapi/FutbolDatosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apicalloguedes.futboldatosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutbolDatosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

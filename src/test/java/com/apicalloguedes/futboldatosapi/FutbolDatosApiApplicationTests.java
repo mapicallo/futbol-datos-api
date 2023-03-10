3d08f1566e44268e9cf70a0345ffd244b1d576a9
mpicallo <mapicalloperez@hotmail.com>
2023-03-10 13:15:48 +0100

    compila ok contra mysql

--- src/test/java/com/apicalloguedes/futboldatosapi/FutbolDatosApiApplicationTests.java
+++ src/test/java/com/apicalloguedes/futboldatosapi/FutbolDatosApiApplicationTests.java
@@ -3,11 +3,17 @@ package com.apicalloguedes.futboldatosapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class FutbolDatosApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		FutbolDatosApiApplication.main(new String[]{});
+		assertTrue(true);
+
 	}
 
 }
